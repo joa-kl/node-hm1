89f09739ff451b85ccfd552b0c65928b1c2830eb
Joanna K <joannakleinrm@gmail.com>
2023-07-26 23:16:57 +0200

    json import

---
+++ node_hw1/app.js
@@ -0,0 +1,6 @@
+import { info, log } from './import.js';
+
+info('info function');
+log('log function');
+
+
---
+++ node_hw1/import.js
@@ -0,0 +1,9 @@
+export const info = msg => {
+    console.log(`Info: ${msg}`);
+};
+
+export const log = msg => {
+    console.log(`Log: ${msg}`);
+};
+
+log('log function');
