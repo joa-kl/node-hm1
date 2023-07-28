03b978ec028400d023e1fbe47880109575922f6c
Joanna K <joannakleinrm@gmail.com>
2023-07-28 23:39:23 +0200

    functions in contacts.js added

--- node_hw1/contacts.js
+++ node_hw1/contacts.js
@@ -1,7 +1,8 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-const contactsPath = path.join(__dirname,'./db/contacts.json') ;
+const contactsPath = path.join(__dirname, './db/contacts.json');
+const contactsDataBase = require("./db/contacts.json");
 
 function parseContacts(data) {
     return JSON.parse(data.toString());
@@ -42,12 +43,69 @@ function getContactById(contactId) {
         .catch((err) => console.log(err.message));
 }
 
-getContactById("drsAJ4SHPYqZeG-83QTVv");
+// getContactById("drsAJ4SHPYqZeG-83QTVv");
 
 function removeContact(contactId) {
-    
+    fs.readFile(contactsPath)
+        .then((data) => {
+            const contacts = parseContacts(data);
+            return contacts;
+        })
+        .then((contacts) => {
+            const contactIndex = contacts.findIndex(
+                (contact) => contact.id === contactId
+            );
+            if (contactIndex !== -1) {
+                contacts.splice(contactIndex, 1);
+                fs.writeFile(contactsPath, JSON.stringify(contacts), (error) => {
+                    if (error) {
+                        console.log(error.message);
+                        return;
+                    }
+                });
+                console.log(
+                    `Contact with the id ${contactId} has been removed.`.green
+                );
+            } else {
+                console.log(`There is no contact with the id: ${contactId}.`.red)
+            }
+        })
+        .catch ((error) => console.log(error.message));
 }
 
 function addContact(name, email, phone) {
-    
+    const contact = {
+        id: (
+            Math.floor(Math.random() * 100000) + contactsDataBase.length
+        ).toString(),
+        name,
+        email,
+        phone
+    }
+
+    if (name === undefined || email === undefined || phone === undefined) {
+        console.log("Please set all arguments (name, email, phone) to add contact".red);
+        return;
+    }
+
+    contactsDataBase.push(contact);
+    const contactsUpdate = JSON.stringify(contactsDataBase);
+
+    fs.writeFile(contactsPath, contactsUpdate, (error) => {
+        if (error) {
+            console.log("Oops, something went wrong:".red, error.message);
+            return;
+        }
+    });
+    console.log(`${name} has been added to your contacts`.green);
+
 }
+
+addContact("Franek", "Franek@warzywo", "12334566");
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+};
